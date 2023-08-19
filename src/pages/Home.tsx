fa38104edcb7320b40b1aee6fad23b8f46a798fa
Yeonseo-Jo <jys073190@gmail.com>
2023-08-19 21:15:01 +0900

    init : 스타일드 컴포넌트 테스트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,16 @@
+import { styled } from "styled-components";
+
 const Home = () => {
-  return <div>HacKuthon2023-frontend</div>;
+  return (
+    <>
+      <div>HacKuthon2023-frontend</div>;
+      <TestTitle>메인 홈 경로입니다</TestTitle>
+    </>
+  );
 };
 
 export default Home;
+
+const TestTitle = styled.h1`
+  color: blue;
+`;
