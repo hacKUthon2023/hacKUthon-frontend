95e0591f3b684fc28c17da7dc3987e8fab423edf
Yeonseo-Jo <jys073190@gmail.com>
2023-08-21 03:02:14 +0900

    feat : 스타일 가이드 추가

---
+++ src/styles/style.d.ts
@@ -0,0 +1,13 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      primary_green: string;
+      primary_gray: string;
+      primary_black: string;
+      sub_red: string;
+      sub_yello2: string;
+    };
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+const colors = {
+  primary_green: "#3B872F",
+  primary_gray: "#979C9E",
+  primary_black: "#000000",
+  sub_red: "#D3180C",
+  sub_yello2: "#FFB323",
+};
+
+const theme: DefaultTheme = {
+  colors,
+};
+
+export default theme;
