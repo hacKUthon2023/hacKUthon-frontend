301225ee70fc24b266dfe5de74ef09bfe187493a
Yeonseo-Jo <jys073190@gmail.com>
2023-08-21 04:56:06 +0900

    feat : 경로 페이지 ui

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,2 +1,3 @@
 export { ReactComponent as IcMainLogo } from "./IcMainLogo.svg";
 export { ReactComponent as IcBacBtn } from "./IcBackBtn.svg";
+export { ReactComponent as IcRouteArrow } from "./IcRouteArrow.svg";
--- src/pages/SubWayRoute.tsx
+++ src/pages/SubWayRoute.tsx
@@ -1,13 +1,19 @@
 import { styled } from "styled-components";
 import PageLayout from "../components/common/PageLayout";
+import { IcRouteArrow } from "../assets/icon";
 
 const SubWayRoute = () => {
   return (
     <PageLayout>
       <SubWayRouteWrapper>
-        <RouteInfoBox>출발역</RouteInfoBox>
-        <RouteInfoBox>환승역</RouteInfoBox>
-        <RouteInfoBox>도착역</RouteInfoBox>
+        <RouteInfoBoxContainer>
+          <ThirdInfoBox>경복궁</ThirdInfoBox>
+          <IcRouteArrow />
+          <TransferInfoBox>을지로3가</TransferInfoBox>
+          <IcRouteArrow />
+          <TwoLineInfoBox>문래</TwoLineInfoBox>
+        </RouteInfoBoxContainer>
+        <SubWayRouteBtn>차량번호 입력하기</SubWayRouteBtn>
       </SubWayRouteWrapper>
     </PageLayout>
   );
@@ -18,16 +24,58 @@ export default SubWayRoute;
 const SubWayRouteWrapper = styled.section`
   display: flex;
   flex-direction: column;
-  gap: 3rem;
+  align-items: center;
+`;
+
+const RouteInfoBoxContainer = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.8rem;
+  margin: 1.2rem 0;
 `;
 
 const RouteInfoBox = styled.div`
+  width: fit-content;
+  padding: 0.8rem 1.6rem;
+
+  color: #fff;
+
+  border-radius: 4.8rem;
+
+  font-size: 1.6rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 1.6rem;
+`;
+
+const TwoLineInfoBox = styled(RouteInfoBox)`
+  background-color: #60b257;
+`;
+
+const ThirdInfoBox = styled(RouteInfoBox)`
+  background-color: #ee7b30;
+`;
+
+const TransferInfoBox = styled(RouteInfoBox)`
+  background-color: #202325;
+`;
+
+const SubWayRouteBtn = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: gray;
-  width: 9rem;
-  height: 5rem;
 
-  border-radius: 3rem;
+  width: 32.7rem;
+  height: 4.8rem;
+
+  background-color: ${({ theme }) => theme.colors.primary_base};
+
+  border-radius: 4.8rem;
+
+  color: #fff;
+  font-size: 1.6rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 1.6rem;
 `;
